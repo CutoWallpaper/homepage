7b9c6c449587d3a017d01459bd9c362f718aec2d
icodesign <git@icodesign.me>
2023-10-30 15:41:47 +0800

    Add actionmate privacy

---
+++ app/[locale]/(page)/actionmate/privacy/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: { locale: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const locale = "en";
+  const { meta } = await import(`./privacy.${locale}.mdx`);
+  return {
+    title: meta.title,
+    description: meta.description,
+  };
+}
+
+export default async function Faq({ params }: Props) {
+  const { default: Content } = await import(`./privacy.en.mdx`);
+  return <Content />;
+}

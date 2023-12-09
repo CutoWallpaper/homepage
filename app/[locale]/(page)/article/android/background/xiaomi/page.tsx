7865f23e0810cf42e6357545affb8c1e6cf5cb21
icodesign <git@icodesign.me>
2023-12-09 16:03:16 +0800

    Add xiaomi background article

---
+++ app/[locale]/(page)/article/android/background/xiaomi/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+
+type Props = {
+  params: { locale: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const locale = "zh";
+  const { meta } = await import(`./article.${locale}.mdx`);
+  return {
+    title: meta.title,
+    description: meta.description,
+  };
+}
+
+export default async function Faq({ params }: Props) {
+  const { default: Content } = await import(`./article.zh.mdx`);
+  return <Content />;
+}
